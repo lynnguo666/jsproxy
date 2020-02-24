5d54ea6ea7884d752d063762f55b4b31f8709141
lynnguo666 <39556167+lynnguo666@users.noreply.github.com>
2020-02-24 14:24:50 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '137',
+  ver: '138',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -29,19 +29,19 @@ jsproxy_config({
     'test-01': {
       label: '节点1',
       lines: {
-        'jsproxy.7fhvj32a.workers.dev': 1,
+        'jsproxy.7fhvj32a.workers.dev：8443': 1,
       }
     },
         'test-02': {
       label: '节点2',
       lines: {
-        'damp-bush-9bec.zero-one.workers.dev': 1,
+        'damp-bush-9bec.zero-one.workers.dev：8443': 1,
       }
     },
             'test-03': {
       label: '节点3',
       lines: {
-        'jsproxy.zero-one.workers.dev': 1,
+        'jsproxy.zero-one.workers.dev:8443': 1,
       }
     },
     'mysite': {
